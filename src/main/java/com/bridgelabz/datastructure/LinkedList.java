02ac2cdada9d1652aa5388f8128a3c8dfdf9031d
vijikumar123 <vk9164940756@gmail.com>
2023-02-02 09:33:53 +0530

    Added:uc5 LinkedList Problems

--- src/main/java/com/bridgelabz/datastructure/LinkedList.java
+++ src/main/java/com/bridgelabz/datastructure/LinkedList.java
@@ -3,22 +3,24 @@ package com.bridgelabz.datastructure;
 public class LinkedList<T> {
     Node<T> head;
     Node<T> tail;
-    public void push(T key){
+
+    public void push(T key) {
         Node<T> newNode = new Node<>(key);
-        if (head == null){
-            head=newNode;
-            tail= newNode;
-        }else {
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
             newNode.next = head;
             head = newNode;
         }
     }
+
     public void display() {
         Node<T> temp = head;
         while (temp != null) {
-            if (temp.next == null){
+            if (temp.next == null) {
                 System.out.print(temp.key);
-            }else {
+            } else {
                 System.out.print(temp.key + " -> ");
             }
             temp = temp.next;
@@ -26,19 +28,18 @@ public class LinkedList<T> {
         System.out.println();
     }
 
-    public void insert(T key,int position){
+    public void insert(T key, int position) {
         //Create a new node
         Node<T> newNode = new Node(key);
-        if(head == null) {
+        if (head == null) {
             //If list is empty, both head and tail would point to new node
             head = newNode;
             tail = newNode;
-        }
-        else {
+        } else {
             Node temp, current;
             temp = head;
             current = null;
-            for(int i = 0; i < position; i++) {
+            for (int i = 0; i < position; i++) {
                 //Node current will point to temp
                 current = temp;
                 //Node temp will point to node next to it.
@@ -51,7 +52,24 @@ public class LinkedList<T> {
         }
 
     }
+
+    public void  pop() {
+        if (head == null) {
+            System.out.println("List is empty");
+
+        } else {
+
+            if (head != tail) {
+                head = head.next;
+            }
+            else {
+                head = tail = null;
+            }
+        }
+    }
+
 }
+
 
 
 
--- src/main/java/com/bridgelabz/datastructure/LinkedListMain.java
+++ src/main/java/com/bridgelabz/datastructure/LinkedListMain.java
@@ -6,18 +6,16 @@ public class LinkedListMain {
     public static void main(String[] args) {
         LinkedList<Integer> linkedList1 = new LinkedList<>();
         linkedList1.push(70);
+        linkedList1.push(30);
         linkedList1.push(56);
         linkedList1.display();
 
-//	        linkedList1.push(56);
-//	        linkedList1.push(30);
-//	        linkedList1.push(70);
-//	        linkedList1.display();
-
-        linkedList1.insert(30,1);
+        linkedList1.pop();
         linkedList1.display();
     }
+
 }
+
 
 
 
