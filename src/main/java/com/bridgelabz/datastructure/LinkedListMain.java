c044f197b03968b4a598ec99504789184996bf97
vijikumar123 <vk9164940756@gmail.com>
2023-02-02 09:10:17 +0530

    Added:uc1 LinkedList Problems

--- src/main/java/com/bridgelabz/datastructure/LinkedListMain.java
+++ src/main/java/com/bridgelabz/datastructure/LinkedListMain.java
@@ -1,22 +1,11 @@
 package com.bridgelabz.datastructure;
 
-import java.util.LinkedList;
 public class LinkedListMain {
     public static void main(String[] args) {
         LinkedList<Integer> linkedList1 = new LinkedList<>();
         linkedList1.push(70);
         linkedList1.push(30);
         linkedList1.push(56);
-        linkedList1.display();
-
-	            linkedList1.pop();
-	            linkedList1.display();
-
-	            linkedList1.popLast();
-	            linkedList1.display();
-        linkedList1.searchNode(30);
-        linkedList1.searchAndInsert(30,40);
-        linkedList1.display();
-
     }
 }
+
